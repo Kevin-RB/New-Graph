1dac14d60f49f96da12e4e47758ada35521809df
Kevin Romero Bedoya <kr38996@gmail.com>
2022-01-06 20:29:50 -0500

    nomalized data, first tooltips

--- bubbles/script.js
+++ bubbles/script.js
@@ -12,12 +12,14 @@ let interceptionSpace = ((r2 - r1) / 2)
 let intercept1 = r1
 let intercept2 = r1 + interceptionSpace
 let intercept3 = r1 + 2 * interceptionSpace
+let intercept4 = intercept3 + 50
 
 //helper radius function
 const helperRadiusFunction = {
     0: intercept1,
     1: intercept2,
-    2: intercept3
+    2: intercept3,
+    label: intercept4
 }
 
 //division space for the graph
@@ -64,6 +66,7 @@ d3.csv('../data.csv').then(data => {
 
     drawCanvas()
     generateRadialLines(variables)
+    // generateLabels(variables)
     generateCircles()
     renderData(indexedVariables)
 })
@@ -101,19 +104,49 @@ const generateRadialLines = (variables, data) => {
         .attr("stroke", "gray")
         .attr('stroke-width', 2)
         .attr('transform', `translate(${w / 2}, ${h / 2})`)
+        .append('title')
+        .text((d) => d)
+    // .on('mouseover', (item) => {
+    //     console.log(item.target.__data__)
+    // })
+}
+
+const generateLabels = (variables) => {
+    console.log(variables)
+    //intial angle
+    let angle = (Math.PI / 180) * 90
+    for (let item of variables) {
+        console.log(item)
+        svg.select(`#${item}`)
+            .append("text")
+            .attr("x", () => {
+                const xCenter = w / 2
+                const xPos = xCenter + helperRadiusFunction[label] * Math.cos(angle) - 45 / 2
+                return xPos
+            })
+            .attr("y", () => {
+                const xCenter = w / 2
+                const xPos = xCenter + helperRadiusFunction[label] * Math.sin(angle)
+                return xPos
+            })
+            .text(item);
+        angle -= divisionSpace
+    }
 }
 
 const renderData = (chartData) => {
     const keys = Object.keys(chartData)
     console.log(chartData)
-
+    //min-max values for normalization 
+    const [minVal, maxVal] = getMinMax(chartData)
+    console.log(minVal, maxVal)
     //intial angle
     let angle = (Math.PI / 180) * 90
 
     for (let variable in keys) {
-        console.log(keys[variable])
+        // console.log(keys[variable])
         const data = chartData[keys[variable]]
-        console.log(data)
+        // console.log(data)
         svg.select(`#${keys[variable]}`)
             .selectAll('circle')
             .data(data)
@@ -121,19 +154,39 @@ const renderData = (chartData) => {
             .append('circle')
             .attr('cx', (d, i) => {
                 const xCenter = w / 2
-                console.log(d, i)
                 const xPos = xCenter + helperRadiusFunction[i] * Math.cos(angle)
                 return xPos
             })
             .attr('cy', (d, i) => {
                 const yCenter = h / 2
-                console.log(d, i)
                 const yPos = yCenter - helperRadiusFunction[i] * Math.sin(angle)
                 return yPos
             })
-            .attr('r', 20)
+            .attr('r', (d) => {
+                if (d != undefined) {
+                    // console.log(d)
+                    return normalize(d, minVal, maxVal)
+                }
+            })
             .attr('data', (d) => d)
-            .attr("fill", "pink")
+            .attr("fill", `pink`)
+            .attr('stroke', 'black')
+            // .attr("fill", `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`)
+            .on("mouseover", (d) => {
+                console.log(d)
+                const data = d.target.__data__
+                const variable = d.target.parentNode.__data__
+                tooltip.transition()
+                    .style("visibility", "visible")
+                    .text(`${variable}, ${data}`)
+            })
+            .on("mousemove", (d) => {
+                tooltip.style("top", (d.pageY - 10) + "px")
+                    .style("left", (d.pageX + 10) + "px");
+            })
+            .on("mouseout", () => {
+                tooltip.style("visibility", "hidden")
+            });
         angle -= divisionSpace
     }
 }
@@ -152,4 +205,34 @@ const generateCircles = () => {
         .attr('stroke-width', 2)
         .attr("stroke", "gray")
         .attr("fill", "none")
+}
+
+let tooltip = d3.select('body')
+    .append('div')
+    .attr('id', 'tooltip')
+    .style('position', 'absolute')
+    .style('z-index', '10')
+    .style('width', 'auto')
+    .style('height', 'auto')
+    .style('visibility', 'hidden')
+    .text('dummy xd 420')
+
+const randomColor = () => {
+    return Math.random() * 255;
+}
+
+const getMinMax = (data) => {
+    const dataArray = []
+    for (const key in data) {
+        data[key].forEach(value => dataArray.push(value))
+    }
+    const min = Math.min(...dataArray)
+    const max = Math.max(...dataArray)
+    return [min, max]
+}
+
+const normalize = (x, min, max) => {
+    const augmentFactor = 60
+    const normalized = augmentFactor * (x - min) / (max - min)
+    return normalized
 }
